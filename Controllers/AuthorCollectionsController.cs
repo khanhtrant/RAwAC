f42d8197db40e8ed532f68ae38f1d6a01275c498
Khanh Tran <khanhttnl@gmail.com>
2018-02-10 16:18:03 +0700

    Add CreateAuthorCollections

---
+++ Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RESTful.Models;
+using RESTful.Services;
+
+namespace RESTful.Controllers
+{
+    [Route("api/authorcollections")]
+    public class AuthorCollectionsController:Controller
+    {
+        private ILibraryRepository _libraryRepository;
+
+        public AuthorCollectionsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository=libraryRepository;
+        }
+
+        [HttpPost]
+        public IActionResult CreateAuthorCollection([FromBody]IEnumerable<AuthorForCreationDto>authorCollection)
+        {
+            if (authorCollection==null)
+            {
+                return BadRequest();
+            }
+
+            var authorEntities=Mapper.Map<IEnumerable<Entities.Author>>(authorCollection);
+            foreach (var author in authorEntities)
+            {
+                _libraryRepository.AddAuthor(author);
+            }
+
+            if (!_libraryRepository.Save())
+            {
+                return StatusCode(500,"Unexpected error from server");
+            }
+
+            return Ok();
+        }
+    }
+}
